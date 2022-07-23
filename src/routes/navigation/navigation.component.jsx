ff75e4074c78ea423eff360e6b52062b20b4fbd6
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-07-22 23:07:24 -0400

    created user context, wrapped out app with the provider and added to sign in page and nagivation

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,12 @@
 import { Outlet, Link } from 'react-router-dom';
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import './navigation.styles.scss';
+import { UserContext } from '../../contexts/user.context';
 
 export const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+  console.log(currentUser);
   return (
     <>
       <div className="navigation">
