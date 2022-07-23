01d4975ad85f0942a2555518d0a42c1a48dae125
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-07-23 19:05:22 -0400

    created checkout page, checkout items and checkout cart functionality

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './routes/home/home.component.jsx';
 import './categories.styles.scss';
 import { Navigation } from './routes/navigation/navigation.component.jsx';
 import Authentication from './routes/authentication/authentication.component';
-
+import Checkout from './routes/checkout/checkout.component.jsx';
 import Shop from './routes/shop/shop.component.jsx';
 
 const App = () => {
@@ -13,6 +13,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
