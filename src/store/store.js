a78d6fa60015c8d982acc175796607f3013a886f
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-07-26 13:58:22 -0400

    complete thunk integration

--- src/store/store.js
+++ src/store/store.js
@@ -2,8 +2,20 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
+
+const middleWares = [
+  process.env.NODE_ENV === 'development' && logger,
+  thunk,
+].filter(Boolean);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 const persistConfig = {
   key: 'root',
@@ -13,18 +25,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
-  Boolean
-);
-
-const composeEnhancers =
-  compose(
-    process.env.NODE_ENV !== 'production' &&
-      window &&
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  ) || compose;
-
-const composedEnhancers = composeEnhancers(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer,
