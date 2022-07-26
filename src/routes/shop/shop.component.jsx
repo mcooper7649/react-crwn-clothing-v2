34d1437f71a8784e9638cad18359ae3dc6f2757d
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-07-26 11:18:00 -0400

    complete redux installation and removed all references of context

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,26 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils.js';
-import { setCategoriesMap } from '../../store/categories/categories.action.js';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import { setCategories } from '../../store/categories/category.action';
 
 const Shop = () => {
   const dispatch = useDispatch();
+
   useEffect(() => {
     const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments('categories');
-      dispatch(setCategoriesMap(categoryMap));
+      const categoriesArray = await getCategoriesAndDocuments('categories');
+      dispatch(setCategories(categoriesArray));
     };
 
     getCategoriesMap();
   }, []);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
-      <Route path=":category" element={<Category />} />
+      <Route path=':category' element={<Category />} />
     </Routes>
   );
 };
--- src/store/categories/categories.types.js
+++
@@ -1,3 +0,0 @@
-export const CATEGORIES_ACTION_TYPE = {
-  SET_CATEGORIES_MAP: 'category/SET_CATEGORIES_MAP',
-};
